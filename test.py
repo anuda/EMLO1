c95a0bd94c104e3f9c562fc0a5bccaff8ef6c02c
anuda <anuda@DESKTOP-9RG8N35.localdomain>
2021-09-22 21:35:26 +0530

    new test case

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ import pytest
 import pandas as pd
 import os
 
+
+def check_sum():
+    assert(2+3==5)
+
 def check_existance():
     files = os.listdir()
     print(files)
     assert 'data' not in files
-
-
-def check_sum():
-    assert(2+3==5)
